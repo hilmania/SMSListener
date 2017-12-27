4ffefb3471a608a4659b46564f841353f78c758e
Ibnu Shidq <hia.develop@gmail.com>
2017-12-27 15:47:21 +0700

    Initial commit

---
+++ app/src/main/java/com/hilmania/smslistenerapp/SmsReceiverActivity.java
@@ -0,0 +1,13 @@
+package com.hilmania.smslistenerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmsReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_receiver);
+    }
+}
